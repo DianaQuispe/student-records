e5319a3f6bc70cc95ceec8e631af69b63f817a70
MyDiana <danaalienmas@gmail.com>
2017-09-21 05:23:20 -0500

    avance

--- js/literal.js
+++ js/literal.js
@@ -1,6 +1,9 @@
+'use strict';
 
- const app {
+const app {
+    init:  {
 
+    }
     function Person(name,techPoints,lifePoints,status ) {
         this.name = undefined;
         this.techPoints = undefined;
@@ -10,54 +13,41 @@
     }
     var students = [];
     
-    $('#add').click(function() {
-      
+    function printOne() {
         students.push({
             name : prompt('Student Name:', 'Name'),
             techPoints : prompt('Tech Points:',' 70%'),
             lifePoints : prompt('Life Points:','70%'),
             status : 'active',
         })
-        console.log(students);
-        $('#student').html('');
-        $('#student').append(`<div class="addStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${students[students.length-1].name }</p><p><b>Tech Points</b>:${students[students.length-1].techPoints }</p><p><b>Life Points:</b>${students[students.length-1].lifePoints }</p><p><b>Status:</b>${students[students.length-1].status }</p></div>`)
-    
-    })
-    
-     $('#print').click(printAll);
+        $('#student').empty();  
+      $('#student').append(`<div class="addStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${students[students.length-1].name }</p><p><b>Tech Points</b>:${students[students.length-1].techPoints }</p><p><b>Life Points:</b>${students[students.length-1].lifePoints }</p><p><b>Status:</b>${students[students.length-1].status }</p></div>`)
+    }
+    $('#add').click(printOne);
+    $('#print').click(printAll);
+    $('#update').click(checkNotes);
+    $('#run').click(checkNotes);
     
     function printAll(){
-        function map() {
-            $('#student').html('');
-            $('#student').append(students.map(printAll));
-            
-        }
-        map();
-        function printAll(item) {
-            return `<div class="printStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${item.name }</p><p><b>Tech Points</b>:${item.techPoints }</p><p><b>Life Points:</b>${item.lifePoints }</p><p><b>Status:</b>${item.status }</p></div>`;
-        }
+        $('#student').empty();  
+        
+        var result = students.map((value)=>{
+            return $('#student').append(`<div class="printStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${value.name }</p><p><b>Tech Points</b>:${value.techPoints }</p><p><b>Life Points:</b>${value.lifePoints }</p><p><b>Status:</b>${value.status }</p></div>`);
+        } )
+              
+        
+        console.log(result);
+    }
       
-    }
-    
-     $('#update').click(checkNotes);
-    
+     
     function checkNotes() {
-    var newStudent = students.filter(function(number) {
-        let int = parseInt(number.techPoints);
-        let int1 = parseInt(number.lifePoints);
-       let prom = int + int1/ 2;
-        return (prom >= 70)
-        
-    });
-    $('#student').html('');
-    for(var i in newStudent){
-        $('#student').append(`<div class="addStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${newStudent[i].name }</p><p><b>Tech Points</b>:${newStudent[i].techPoints }</p><p><b>Life Points:</b>${newStudent[i].lifePoints }</p><p><b>Status:</b>${newStudent[i].status }</p></div>`)
-        
+     var newStudents =students.filter((value)=>{return (parseInt(value.techPoints)+parseInt(value.lifePoints)/2 >= 70) } )
+     $('#student').empty();  
+     for(var i in newStudents){
+        $('#student').append(`<div class="checkStudents"><h1><b>STUDENT:</b></h1><br><p><b>Name:</b>${newStudents[i].name }</p><p><b>Tech Points</b>:${newStudents[i].techPoints }</p><p><b>Life Points:</b>${newStudents[i].lifePoints }</p><p><b>Status:</b>${newStudents[i].status }</p></div>`);
     }
+    console.log(newStudents);
     
-    
-    console.log(newStudent);
-    
-    }
+    };
  }
  
