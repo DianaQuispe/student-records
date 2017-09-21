0732087e140d085773c259c2cce986174d9d7d4e
MyDiana <danaalienmas@gmail.com>
2017-09-21 06:28:45 -0500

    finalizado con 2 js

--- js/literal.js
+++ js/literal.js
@@ -16,21 +16,21 @@ const app = {
             status : 'active',
         })
         $('#student').empty();  
-      $('#student').append(`<div class="addStudents"><h1><b>STUDENT:</b></h1><p><b>Name:</b>${app.students[app.students.length-1].name }</p><p><b>Tech Points</b>:${app.students[app.students.length-1].techPoints }</p><p><b>Life Points:</b>${app.students[app.students.length-1].lifePoints }</p><p><b>Status:</b>${app.students[app.students.length-1].status }</p></div>`)
+      $('#student').append(`<div class="addStudents"><h3><b>STUDENT:</b></h3><p><b>Name:</b>${app.students[app.students.length-1].name }</p><p><b>Tech Points</b>:${app.students[app.students.length-1].techPoints }</p><p><b>Life Points:</b>${app.students[app.students.length-1].lifePoints }</p><p><b>Status:</b>${app.students[app.students.length-1].status }</p></div>`)
 
     },
     printAll : function() {
         $('#student').empty();  
         
         var result = app.students.map((value)=>{
-            return $('#student').append(`<div class="printStudents"><h1><b>STUDENT:</b></h1><p><b>Name:</b>${value.name }</p><p><b>Tech Points</b>:${value.techPoints }</p><p><b>Life Points:</b>${value.lifePoints }</p><p><b>Status:</b>${value.status }</p></div>`);
+            return $('#student').append(`<div class="printStudents"><h3><b>STUDENT:</b></h3><p><b>Name:</b>${value.name }</p><p><b>Tech Points</b>:${value.techPoints }</p><p><b>Life Points:</b>${value.lifePoints }</p><p><b>Status:</b>${value.status }</p></div>`);
         } )
     },
     checkNotes : function() {
         var newStudents =app.students.filter((value)=>{return (parseInt(value.techPoints)+parseInt(value.lifePoints)/2 >= 70) } )
         $('#student').empty();  
         for(var i in newStudents){
-           $('#student').append(`<div class="checkStudents"><h1><b>STUDENT:</b></h1><p><b>Name:</b>${newStudents[i].name }</p><p><b>Tech Points</b>:${newStudents[i].techPoints }</p><p><b>Life Points:</b>${newStudents[i].lifePoints }</p><p><b>Status:</b>${newStudents[i].status }</p></div>`);
+           $('#student').append(`<div class="checkStudents"><h3><b>STUDENT:</b></h3><p><b>Name:</b>${newStudents[i].name }</p><p><b>Tech Points</b>:${newStudents[i].techPoints }</p><p><b>Life Points:</b>${newStudents[i].lifePoints }</p><p><b>Status:</b>${newStudents[i].status }</p></div>`);
        }
        console.log(newStudents);
     }
